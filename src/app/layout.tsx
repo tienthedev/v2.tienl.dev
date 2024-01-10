a0315c082ebcd16f927dfe8e5529f6d74d93ae10
Tien <leductiendung29@gmail.com>
2024-01-10 12:10:25 -0600

    Redesigned favicon.ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={chivo_mono.className}>
         {children}
         <Analytics/>
