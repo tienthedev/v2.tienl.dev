b07430a50aade76228582daa6842c634e9dbcef3
Tien <leductiendung29@gmail.com>
2024-01-23 14:00:02 -0600

    Redesigned Projects component, deploy for mobile testing

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import Image from 'next/image';  // Import the Image component
 import remyshop from '../../assets/remyshop.png';
@@ -10,93 +11,72 @@ const Projects: React.FC = () => {
     {
       img: remyshop,
       title: 'REMYUS.SHOP',
-      desc: 'AN ONLINE SHOP. BUILT WITH HTML, CSS, JAVASCRIPT, AND BOOTSTRAP.',
-      live: 'https://flannelboy.github.io/remyus.shop/',
+      desc: 'AN ONLINE SHOP',
+      year: '2023',
       code: 'https://github.com/flannelboy/remyus.shop',
     },
     {
       img: tienldev,
       title: 'TIENL.DEV',
-      desc: 'A PERSONAL PORTFOLIO (VERSION 1). BUILT WITH HTML, CSS, JAVASCRIPT, AND BOOTSTRAP.',
-      live: 'https://flannelboy.github.io/tienl.dev/',
+      desc: 'A PERSONAL PORTFOLIO (VERSION 1)',
+      year: '2022',
       code: 'https://github.com/flannelboy/tienl.dev',
     },
     {
       img: tdpos,
       title: 'TD POS',
-      desc: 'A POINT OF SALE SYSTEM (VERSION 1). BUILT WITH JAVA, JAVA SWING, REACT NATIVE.',
-      live: 'https://github.com/flannelboy/PointOfSales',
+      desc: 'A POINT OF SALE SYSTEM (VERSION 1)',
+      year: '2023',
       code: 'https://github.com/flannelboy/PointOfSales',
     },
     {
       img: invento,
       title: 'INVENTO/TD LIBRARY',
-      desc: 'AN INVENTORY MANAGEMENT SYSTEM (VERSION 1, 2). BUILT WITH JAVA, JAVA SWING.',
-      live: 'https://flannelboy.github.io/tienl.dev/',
+      desc: 'AN INVENTORY MANAGEMENT SYSTEM (VERSION 1, 2)',
+      year: '2023',
       code: 'https://github.com/flannelboy/tienl.dev',
     },
 
   ];
 
   return (
-    <section className="bg-secondary text-primary px-5 py-32" id="projects">
+    <div className="bg-secondary text-primary px-5 py-32" id="projects">
       <div className="container mx-auto grid md:grid-cols-2 items-center md:justify-between">
         <div className="about-info mb-5">
           <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-fit border-secondary pb-2">
             Projects
           </h2>
-
-          <p className="pb-5">
-            HOVER ON THE IMAGE TO SEE THE LIVE DEMO AND SOURCE CODE.
-          </p>
         </div>
-
-        <div className="about-img"></div>
       </div>
 
-      <div className="projects container mx-auto grid md:grid-cols-3 gap-10">
-        {projects.map((project, i) => {
-          return (
-            <div className="relative" key={i}>
-              <Image
-                src={project.img}
-                alt={project.title}
-                width={500}
-                height={500}
-              />
-              <div className="flex absolute left-0 right-0 top-[13px] bottom-0 mx-auto w-[90%] h-[90%]  bg-primary  opacity-0 duration-500 justify-center flex-col hover:opacity-100 ">
-                <p className="py-5 text-center  px-2 text-secondary">
-                  {project.title}
-                </p>
-
-                <p className="py-5 text-center  px-2 text-secondary">
-                  {project.desc}
-                </p>
-
-                <div className="mx-auto">
-                  <a
-                    href={project.live}
-                    className="px-5 py-2 text-primary bg-white hover:bg-secondary mr-5 font-bold"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Live
-                  </a>
+      <div className="container mx-auto grid gap-10">
+        <table className="w-full">
+          <tbody>
+            <tr className='font-bold'>
+              <td>Year</td>
+              <td>Project</td>
+              <td>Link</td>
+            </tr>
+            {projects.map((project, i) => (
+              <tr key={i}>
+                <td>{project.year}</td>
+                <td>{project.title}</td>
+                <td>
                   <a
                     href={project.code}
-                    className="px-5 py-2 text-primary bg-white hover:bg-secondary font-bold"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    Code
+                    GitHub
                   </a>
-                </div>
-              </div>
-            </div>
-          );
-        })}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
       </div>
-    </section>
+    </div>
   );
 };
 
